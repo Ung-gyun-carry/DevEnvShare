4b86c21828d82bdcc4aa4bad199ac014fe423e8e
EclipseGood <rjsgml9042@gmail.com>
2023-05-28 17:46:54 +0900

    DB연동 테스트

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $('#loginBtn').click(function () {
+        const id = $('#uid').val();
+        const pw = $('#upw').val();
+        
+        if (id === undefined || id === '') {
+            swal("필수입력", "아이디는 필수입력입니다.", "warning");
+            return;
+        }
+
+        if (pw === undefined || pw === '') {
+            swal("필수입력", "비밀번호는 필수입력입니다.", "warning");
+            return;
+        }
+        const loginObj = {
+            id: id,
+            pw: pw
+        };
+        $.ajax({
+            type: "POST",
+            url: "/user/loginUser",
+            data: loginObj,
+            dataType: "json",
+            success: function (data, textStatus, xhr) {
+                if (data.CNT === 0) {
+                    swal("로그인 실패", "아이디 혹은 패스워드가 일치하지않습니다.", "warning");
+                } else {
+                    swal("로그인 성공", "메인페이지로 이동합니다.", "success");
+                    // window.location.href = 'main.jsp';
+                }
+            },
+            error: function (request, status, error) {
+                alert("code:" + request.status + "\n" + "error:" + error);
+            }
+        })
+    });
+})
